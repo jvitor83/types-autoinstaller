574c4e7e521898c4e015c1d99ff291538916752e
tamj0rd2 <tamara.jordan1+coding@hotmail.com>
2017-08-07 20:50:08 +0100

    Fix tslint errors

--- src/PackageWatcher.ts
+++ src/PackageWatcher.ts
@@ -1,3 +1,5 @@
+import * as _ from "lodash";
+
 export interface Dependency {
     [packageName: string]: string;
 }
@@ -8,103 +10,85 @@ export interface Package {
     devDependencies: Dependency;
 }
 
-interface DetectedChangesCallback {
-    (newPackages: Package, deletedPackes: Package): any;
-}
+type DetectedChangesCallback = (newPackages: Package, deletedPackes: Package) => any;
 
 export class PackageWatcher {
     constructor(private packageJson: Package) {
     }
 
-    changed(changedPackage: Package, detectedChangesCallback: DetectedChangesCallback) {
-        let newPackages: Package = { dependencies: {}, devDependencies: {}, engines: {} };
-        let deletedPackes: Package = { dependencies: {}, devDependencies: {}, engines: {} };
+    public changed(changedPackage: Package, detectedChangesCallback: DetectedChangesCallback) {
+        const newPackages: Package = { dependencies: {}, devDependencies: {}, engines: {} };
+        const deletedPackes: Package = { dependencies: {}, devDependencies: {}, engines: {} };
 
-        //engines
-        for (let key in changedPackage.engines) {
+        // engines
+        for (const key in changedPackage.engines) {
             if (this.exisitsPackage(this.packageJson.engines, key)) {
                 newPackages.engines[key] = changedPackage.engines[key];
             }
         }
 
-        if(this.packageJson.engines == undefined)
-        {
-            for (let key in changedPackage.engines) 
-            {
-                newPackages.engines[key] = changedPackage.engines[key];
-            }
+        if (this.packageJson.engines === undefined) {
+            _.forOwn(changedPackage.engines, (value, key) => {
+                newPackages.engines[key] = value;
+            });
         }
 
-
-        if(changedPackage.engines == undefined)
-        {
-            for (let key in this.packageJson.engines) 
-            {
-                deletedPackes.engines[key] = this.packageJson.engines[key];
-            }
+        if (changedPackage.engines === undefined) {
+            _.forOwn(this.packageJson.engines, (value, key) => {
+                deletedPackes.engines[key] = value;
+            });
         }
 
-
-        for (let key in this.packageJson.engines) {
+        for (const key in this.packageJson.engines) {
             if (this.exisitsPackage(changedPackage.engines, key)) {
                 deletedPackes.engines[key] = this.packageJson.engines[key];
             }
         }
-        //engines
+        // engines
 
-        for (let key in changedPackage.dependencies) {
+        for (const key in changedPackage.dependencies) {
             if (this.exisitsPackage(this.packageJson.dependencies, key)) {
                 newPackages.dependencies[key] = changedPackage.dependencies[key];
             }
         }
 
-        for (let key in changedPackage.devDependencies) {
+        for (const key in changedPackage.devDependencies) {
             if (this.exisitsPackage(this.packageJson.devDependencies, key)) {
                 newPackages.devDependencies[key] = changedPackage.devDependencies[key];
             }
         }
 
-        if(this.packageJson.devDependencies == undefined)
-        {
-            for (let key in changedPackage.devDependencies) 
-            {
-                newPackages.devDependencies[key] = changedPackage.devDependencies[key];
-            }
+        if (this.packageJson.devDependencies === undefined) {
+            _.forOwn(changedPackage.devDependencies, (value, key) => {
+                newPackages.devDependencies[key] = value;
+            });
         }
 
-
-        if(this.packageJson.dependencies == undefined)
-        {
-            for (let key in changedPackage.dependencies) 
-            {
-                newPackages.dependencies[key] = changedPackage.dependencies[key];
-            }
+        if (this.packageJson.dependencies === undefined) {
+            _.forOwn(changedPackage.dependencies, (value, key) => {
+                newPackages.dependencies[key] = value;
+            });
         }
 
-
-        if(changedPackage.dependencies == undefined)
-        {
-            for (let key in this.packageJson.dependencies) 
-            {
-                deletedPackes.dependencies[key] = this.packageJson.dependencies[key];
-            }
+        if (changedPackage.dependencies === undefined) {
+            _.forOwn(this.packageJson.dependencies, (value, key) => {
+                deletedPackes.dependencies[key] = value;
+            });
         }
 
-        if(changedPackage.devDependencies == undefined)
-        {
-            for (let key in this.packageJson.devDependencies) 
-            {
+        if (changedPackage.devDependencies === undefined) {
+            _.forOwn(this.packageJson.devDependencies, (value, key) => {
                 deletedPackes.devDependencies[key] = this.packageJson.devDependencies[key];
-            }
+            });
         }
 
-        for (let key in this.packageJson.dependencies) {
+        for (const key in this.packageJson.dependencies) {
             if (this.exisitsPackage(changedPackage.dependencies, key)) {
                 deletedPackes.dependencies[key] = this.packageJson.dependencies[key];
             }
         }
 
-        for (let key in this.packageJson.devDependencies) {
+        for (const key in this.packageJson.devDependencies) {
             if (this.exisitsPackage(changedPackage.devDependencies, key)) {
                 deletedPackes.devDependencies[key] = this.packageJson.devDependencies[key];
             }
@@ -115,8 +99,7 @@ export class PackageWatcher {
     }
 
     private exisitsPackage(dependencies: Dependency, key: string): boolean {
-        if(dependencies != null)
-        {
+        if (dependencies != null) {
             return !dependencies.hasOwnProperty(key);
         }
         return false;
